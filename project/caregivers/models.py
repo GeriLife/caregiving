a2e9e1ddf03aa72072bb6edef98d0d05c1f2d1c9
Brylie Christopher Oxley <brylie@amble.fi>
2022-04-08 11:55:56 +0300

    Initial CaregiverRole model

--- project/caregivers/models.py
+++ project/caregivers/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class CaregiverRole(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self) -> str:
+        return self.name
