2b2a5451630671744113d33bcde152c443689e8e
Brylie Christopher Oxley <brylie@amble.fi>
2022-04-08 11:54:17 +0300

    Initial DutyType model

--- project/duties/models.py
+++ project/duties/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class DutyType(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self) -> str:
+        return self.name
