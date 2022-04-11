01b02c32269f2ca39a05c5573e4ceeef2e3e0fef
Brylie Christopher Oxley <brylie@amble.fi>
2022-04-11 16:33:49 +0300

    Initial Duties report view, url, and template

--- project/core/urls.py
+++ project/core/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('duties/', include('duties.urls')),
 ]
