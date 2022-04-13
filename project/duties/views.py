330d0ea9fbb93473f2ffc5fb8a9e5221d72cdedc
Brylie Christopher Oxley <brylie@amble.fi>
2022-04-13 14:12:05 +0300

    Parse analytics to list;return dail sum max

--- project/duties/views.py
+++ project/duties/views.py
@@ -12,19 +12,20 @@ class DutiesReportView(TemplateView):
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
 
-        context["duties_daily_sum"] = (
+        context["duties_daily_sum"] = list(
             Duty.objects.values("date")
             .order_by("date")
             .annotate(total_minutes=Sum("duration"))
         )
+        context["duties_daily_sum_max"] = max(daily_sum["total_minutes"] for daily_sum in context["duties_daily_sum"])
 
-        context["duties_type_sum"] = (
+        context["duties_type_sum"] = list(
             Duty.objects.values("type__name")
             .order_by("type__name")
             .annotate(total_minutes=Sum("duration"))
         )
 
-        context["duties_caregiver_role_sum"] = (
+        context["duties_caregiver_role_sum"] = list(
             Duty.objects.values("caregiver_role__name")
             .order_by("caregiver_role__name")
             .annotate(total_minutes=Sum("duration"))
