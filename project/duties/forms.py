f5984614bd78f025cc54ccde99fdf7f6ab49a96f
Brylie Christopher Oxley <brylie@amble.fi>
2022-04-14 11:09:10 +0300

    Set date input type to date

--- project/duties/forms.py
+++ project/duties/forms.py
@@ -7,3 +7,6 @@ class DutyForm(forms.ModelForm):
     class Meta:
         model = Duty
         fields = "__all__"
+        widgets = {
+            "date": forms.DateInput(attrs={"type": "date"}),
+        }
