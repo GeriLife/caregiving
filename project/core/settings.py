a9e7b6ce5e63b05829bde30c7656401d37c3ee7e
Brylie Christopher Oxley <brylie@amble.fi>
2022-04-14 14:36:37 +0300

    Revert BASE_DIR

--- project/core/settings.py
+++ project/core/settings.py
@@ -17,7 +17,7 @@ import environ
 env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
