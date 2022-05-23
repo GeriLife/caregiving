52010a77522e8ac9d40f25ae1448a14b31b49458
Brylie Christopher Oxley <brylie@amble.fi>
2022-05-23 13:17:13 +0300

    Initial resident model

--- project/residents/admin.py
+++ project/residents/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Resident
+
+@admin.register(Resident)
+class ResidentAdmin(admin.ModelAdmin):
+    pass
--- project/residents/models.py
+++ project/residents/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Resident(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_initial = models.CharField(max_length=1)
+    on_hiatus = models.BooleanField(default=False)
+
+    def full_name(self):
+        return f"{ self.first_name } { self.last_initial }"
+
+    class Meta:
+        db_table = "resident"
+        verbose_name = _("resident")
+        verbose_name_plural = _("residents")
+
+    def __str__(self) -> str:
+        return self.full_name()
