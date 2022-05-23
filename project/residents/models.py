a4f88302df2c117c6825f3d22417ec5bf1677f1b
Brylie Christopher Oxley <brylie@amble.fi>
2022-05-23 13:20:09 +0300

    Use UUID for Resident ID

--- project/residents/models.py
+++ project/residents/models.py
@@ -1,8 +1,10 @@
+import uuid
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
 class Resident(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=255)
     last_initial = models.CharField(max_length=1)
     on_hiatus = models.BooleanField(default=False)
