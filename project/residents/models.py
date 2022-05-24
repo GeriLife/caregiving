c7f84574718d71687a561973bc8cb14d92e3fd1e
Brylie Christopher Oxley <brylie@amble.fi>
2022-05-24 15:10:07 +0300

    Add Residency validation (timespan and overlap)

--- project/residents/models.py
+++ project/residents/models.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 import uuid
+
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
@@ -54,3 +56,41 @@ class Residency(models.Model):
 
     def __str__(self) -> str:
         return f"{ self.resident.full_name } - { self.home.name }"
+
+    def clean(self) -> None:
+        """
+        Ensure residency is valid.
+        - move in date preceeds move out date, if present
+        - resident should reside in only one home at a time (no residency overlap)
+        """
+        residency_timespan_is_valid = True
+
+        if self.move_out:
+            # Move in date is before move out (or on same date)
+            residency_timespan_is_valid = self.move_in <= self.move_out
+
+            # No residencies should exist for this resident 
+            # with overlapping move in/out dates (same date is fine)
+            residency_has_overlap = Residency.objects.filter(
+                resident=self.resident,
+                move_in__lt=self.move_out,
+                move_out__gt=self.move_in,
+            ).exists()
+        else:
+            # No residencies should exist for this resident 
+            # with overlapping move out date (same date is fine)
+            residency_has_overlap = Residency.objects.filter(
+                resident=self.resident,
+                move_out__gt=self.move_in,
+            ).exists()
+
+
+        if not residency_timespan_is_valid:
+            error_message = _("Move-in date should preceed move-out date.")
+            raise ValidationError(error_message)
+
+        if residency_has_overlap:
+            error_message = _("Resident can not have overlapping residencies.")
+            raise ValidationError(error_message)
+
+        return super().clean()
