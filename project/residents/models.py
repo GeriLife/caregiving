34ae61e0ab7873db66c627c300b9e381d3d1f517
Brylie Christopher Oxley <brylie@amble.fi>
2022-05-24 14:25:48 +0300

    Initial residency model

--- project/residents/models.py
+++ project/residents/models.py
@@ -1,7 +1,11 @@
+from datetime import datetime
 import uuid
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+
+from homes.models import Home
 
 
 class Resident(models.Model):
@@ -10,6 +14,7 @@ class Resident(models.Model):
     last_initial = models.CharField(max_length=1)
     on_hiatus = models.BooleanField(default=False)
 
+    @property
     def full_name(self):
         return f"{ self.first_name } { self.last_initial }"
 
@@ -19,7 +24,33 @@ class Resident(models.Model):
         verbose_name_plural = _("residents")
 
     def __str__(self) -> str:
-        return self.full_name()
+        return self.full_name
 
     def get_absolute_url(self):
         return reverse("resident-detail", kwargs={"pk": self.pk})
+
+
+class Residency(models.Model):
+    resident = models.ForeignKey(
+        to=Resident,
+        on_delete=models.PROTECT,
+    )
+    home = models.ForeignKey(
+        to=Home,
+        on_delete=models.PROTECT,
+    )
+    move_in = models.DateField(
+        default=timezone.now,
+    )
+    move_out = models.DateField(
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        db_table = "residency"
+        verbose_name = _("residency")
+        verbose_name_plural = _("residencies")
+
+    def __str__(self) -> str:
+        return f"{ self.resident.full_name } - { self.home.name }"
