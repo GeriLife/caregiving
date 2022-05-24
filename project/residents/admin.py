f3da9e94eb9438566cacd8d544b605b8f02e46cc
Brylie Christopher Oxley <brylie@amble.fi>
2022-05-24 14:25:48 +0300

    Initial residency model

--- project/residents/admin.py
+++ project/residents/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
 
-from .models import Resident
+from .models import Residency, Resident
 
 @admin.register(Resident)
 class ResidentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Residency)
+class ResidencyAdmin(admin.ModelAdmin):
     pass
