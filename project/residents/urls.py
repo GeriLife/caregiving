30fd54d84f8be229fddbee824914f0319520d0e3
Brylie Christopher Oxley <brylie@amble.fi>
2022-06-03 11:39:42 +0300

    Add ResidentCreateView

--- project/residents/urls.py
+++ project/residents/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from .views import (
     ResidentDetailView,
+    ResidentCreateView,
     ResidentListView,
 )
 
 urlpatterns = [
+    path("create", ResidentCreateView.as_view(), name="resident-create-view",),
     path("<slug:pk>/", ResidentDetailView.as_view(), name="resident-detail"),
     path("", ResidentListView.as_view(), name="resident-list-view",),
 ]
