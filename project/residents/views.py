4c223bb426d78bbb35b62583f1ec2cf0111e0f4a
Brylie Christopher Oxley <brylie@amble.fi>
2022-06-03 11:07:22 +0300

    Initial Residents ListView

--- project/residents/views.py
+++ project/residents/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 from .models import Resident
 
@@ -8,3 +9,7 @@ class ResidentDetailView(LoginRequiredMixin,DetailView):
     model = Resident
     context_object_name = "resident"
     template_name = "residents/resident_detail.html"
+
+
+class ResidentListView(ListView):
+    model = Resident
