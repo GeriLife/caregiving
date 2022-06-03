ec155951809a330f60ff5052274a5a4f9428f967
Brylie Christopher Oxley <brylie@amble.fi>
2022-06-03 11:07:22 +0300

    Initial Residents ListView

--- project/residents/urls.py
+++ project/residents/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views import (
     ResidentDetailView,
+    ResidentListView,
 )
 
 urlpatterns = [
     path("<slug:pk>/", ResidentDetailView.as_view(), name="resident-detail"),
+    path("", ResidentListView.as_view(), name="resident-list-view",),
 ]
