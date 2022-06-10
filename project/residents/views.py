f321e24d8cd6ad26dd3cd8a115580fd9fef00d7b
Brylie Christopher Oxley <brylie@amble.fi>
2022-06-10 12:04:48 +0300

    Add resident update view

--- project/residents/views.py
+++ project/residents/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
 from django.views.generic.list import ListView
 
 from .models import Resident
@@ -15,6 +16,11 @@ class ResidentDetailView(LoginRequiredMixin,DetailView):
     model = Resident
     context_object_name = "resident"
     template_name = "residents/resident_detail.html"
+
+
+class ResidentUpdateView(LoginRequiredMixin, UpdateView):
+    model = Resident
+    fields = ["first_name", "last_initial", "on_hiatus",]
 
 
 class ResidentListView(ListView):
