cc3c3c0c3f95447d21a4fc9ec4a0fc5486daf9f1
Brylie Christopher Oxley <brylie@amble.fi>
2022-06-10 12:04:48 +0300

    Add resident update view

--- project/residents/urls.py
+++ project/residents/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
 from .views import (
-    ResidentDetailView,
     ResidentCreateView,
+    ResidentDetailView,
     ResidentListView,
+    ResidentUpdateView,
 )
 
 urlpatterns = [
     path("create", ResidentCreateView.as_view(), name="resident-create-view",),
-    path("<slug:pk>/", ResidentDetailView.as_view(), name="resident-detail-view"),
+    path("<slug:pk>/", ResidentDetailView.as_view(), name="resident-detail-view",),
     path("", ResidentListView.as_view(), name="resident-list-view",),
+    path("<slug:pk>/update", ResidentUpdateView.as_view(), name="resident-update-view",),
 ]
