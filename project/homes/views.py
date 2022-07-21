da9684582fc9d0c01c5bd06aa9054af5d7cec931
Brylie Christopher Oxley <brylie@amble.fi>
2022-07-21 15:39:29 +0300

    Initial Homes view and template

--- project/homes/views.py
+++ project/homes/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import Home
+
+class HomeListView(ListView):
+    model = Home
+    context_object_name = "homes"
