81f0512cb8571f7ea0d48e74b18c8a8946cdfaae
Brylie Christopher Oxley <brylie@amble.fi>
2022-07-24 13:21:16 +0300

    Add work_percent_by_role_chart template filter

---
+++ project/homes/templatetags/home_work_percent_by_role.py
@@ -0,0 +1,12 @@
+from django import template
+
+from homes.charts import prepare_home_work_percent_by_caregiver_role_chart
+
+register = template.Library()
+
+@register.filter(name='work_percent_by_role_chart')
+def work_percent_by_role_chart(home):
+    """Returns a chart showing the proportion of work carried out by each role for the given home."""
+    chart = prepare_home_work_percent_by_caregiver_role_chart(home)
+
+    return chart
