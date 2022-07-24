6c933a13f0a1d63903c5a797772e2baabfe49e98
Brylie Christopher Oxley <brylie@amble.fi>
2022-07-24 13:20:08 +0300

    Add Home.get_absolute_url

--- project/homes/models.py
+++ project/homes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 
@@ -12,3 +13,6 @@ class Home(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("home-detail-view", kwargs = {"pk": self.id})
