83de51b0ec44d606178eb912602068e100bfc12f
Brylie Christopher Oxley <brylie@amble.fi>
2022-08-13 14:52:51 +0300

    Add missing i18n

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from django.utils.translation import gettext_lazy as _
 from .views import SignUpView
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
+    path(_("signup/"), SignUpView.as_view(), name="signup"),
 ]
--- project/residents/urls.py
+++ project/residents/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.utils.translation import gettext_lazy as _
 
 from .views import (
     ResidentCreateView,
@@ -8,8 +9,8 @@ from .views import (
 )
 
 urlpatterns = [
-    path("create", ResidentCreateView.as_view(), name="resident-create-view",),
+    path(_("create"), ResidentCreateView.as_view(), name="resident-create-view",),
     path("<slug:pk>/", ResidentDetailView.as_view(), name="resident-detail-view",),
     path("", ResidentListView.as_view(), name="resident-list-view",),
-    path("<slug:pk>/update", ResidentUpdateView.as_view(), name="resident-update-view",),
+    path(_("<slug:pk>/update"), ResidentUpdateView.as_view(), name="resident-update-view",),
 ]
