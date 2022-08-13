9cc065543eda92ed9103833e0c54344c54baa9e5
Brylie Christopher Oxley <brylie@amble.fi>
2022-08-13 15:58:45 +0300

    Remove punctuation from help text; lint

--- project/work/models.py
+++ project/work/models.py
@@ -20,13 +20,32 @@ class WorkType(models.Model):
 
 
 class Work(models.Model):
-    home = models.ForeignKey(Home, related_name="work_performed", on_delete=models.CASCADE, help_text=_("The home in which this work was performed."))
-    type = models.ForeignKey(WorkType, related_name="+", on_delete=models.PROTECT, help_text=_("The general type of work performed."))
-    caregiver_role = models.ForeignKey(CaregiverRole, related_name="+", on_delete=models.PROTECT, help_text=_("The role or job title of the person who performed this work."))
-    date = models.DateField(help_text=_("The date this work was performed."))
-    duration_minutes = models.PositiveIntegerField(help_text=_("The number of minutes used performing this work."))
+    home = models.ForeignKey(
+        Home,
+        related_name="work_performed",
+        on_delete=models.CASCADE,
+        help_text=_("The home in which this work was performed"),
+    )
+    type = models.ForeignKey(
+        WorkType,
+        related_name="+",
+        on_delete=models.PROTECT,
+        help_text=_("The general type of work performed"),
+    )
+    caregiver_role = models.ForeignKey(
+        CaregiverRole,
+        related_name="+",
+        on_delete=models.PROTECT,
+        help_text=_("The role or job title of the person who performed this work"),
+    )
+    date = models.DateField(help_text=_("The date this work was performed"))
+    duration_minutes = models.PositiveIntegerField(
+        help_text=_("The number of minutes used performing this work")
+    )
     duration_hours = models.FloatField(
-        validators=[MinValueValidator(0.0),]
+        validators=[
+            MinValueValidator(0.0),
+        ]
     )
 
     class Meta:
@@ -35,16 +54,15 @@ class Work(models.Model):
         verbose_name_plural = _("work")
         constraints = (
             CheckConstraint(
-                check=Q(duration_hours__gte=0.0),
-                name='work_duration_hours_gte_zero'
+                check=Q(duration_hours__gte=0.0), name="work_duration_hours_gte_zero"
             ),
         )
 
     def get_duration_hours(self):
         minutes_in_hour = 60
-        
+
         return self.duration_minutes / minutes_in_hour
-    
+
     def save(self, *args, **kwargs):
         self.duration_hours = self.get_duration_hours()
         super(Work, self).save(*args, **kwargs)
