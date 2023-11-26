842e4816cc86d05874024b78964cb114a8e2be45
Brylie Christopher Oxley <brylie@protonmail.com>
2023-11-26 06:36:16 +0200

    Remove ActivityType model

--- project/activities/admin.py
+++ project/activities/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from .models import Activity, ActivityType
+from .models import Activity
 
 admin.site.register(Activity)
-
-admin.site.register(ActivityType)
--- project/activities/models.py
+++ project/activities/models.py
@@ -2,23 +2,24 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-class ActivityType(models.Model):
-    name = models.CharField(max_length=25)
-
-    class Meta:
-        db_table = "activity_type"
-        verbose_name = _("activity type")
-        verbose_name_plural = _("activity types")
-
-    def __str__(self):
-        return self.name
-
-
 class Activity(models.Model):
-    activity_type = models.ForeignKey(
-        "ActivityType",
-        on_delete=models.CASCADE,
-        related_name="activities",
+    """
+    Model representing an activity.
+    """
+    class ActivityTypeChoices(models.TextChoices):
+        """
+        Choices for the type of activity.
+        """
+        OUTDOOR = "outdoor", _("Outdoor")
+        CULTURE = "culture", _("Culture")
+        DISCUSSION = "discussion", _("Discussion")
+        SELF_GUIDED = "self_guided", _("Self-guided")
+
+    activity_type = models.CharField(
+        _("Activity type"),
+        max_length=20,
+        choices=ActivityTypeChoices.choices,
+        default=ActivityTypeChoices.SELF_GUIDED,
     )
     residents = models.ManyToManyField(
         "residents.Resident",
