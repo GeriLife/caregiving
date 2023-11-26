0c3c389edf3ca771ab593746700c6f7cbf2e8920
Brylie Christopher Oxley <brylie@protonmail.com>
2023-11-26 06:28:33 +0200

    Install/register/migrate activities

--- project/activities/admin.py
+++ project/activities/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Activity, ActivityType
 
-# Register your models here.
+admin.site.register(Activity)
+
+admin.site.register(ActivityType)
--- project/core/settings.py
+++ project/core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "crispy_forms",
     "crispy_bootstrap5",
     "accounts",
+    "activities",
     "caregivers",
     "homes",
     "residents",
