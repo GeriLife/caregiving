09466a9b1d7ce5b75be8b3ab3828fc9d1b7be920
Brylie Christopher Oxley <brylie@protonmail.com>
2023-11-26 22:16:05 +0200

    Add prepare_activity_hours_by_facilitator_role_chart

--- project/residents/charts.py
+++ project/residents/charts.py
@@ -88,3 +88,45 @@ def prepare_activity_minutes_by_type_chart(
     )
 
     return fig.to_html()
+
+
+def prepare_activity_hours_by_facilitator_role_chart(
+    activities: models.QuerySet[Activity],
+) -> str:
+    """Prepare a bar chart of activity counts by type for a resident."""
+    # This must be a float so that the division below returns a float
+    minutes_in_hour = 60.0
+
+    activities_agg = (
+        activities.values("facilitator_role__title")
+        .annotate(total_hours=models.Sum("duration_minutes") / minutes_in_hour)
+        .order_by("facilitator_role")
+    )
+
+    df_activities = pd.DataFrame(activities_agg)
+
+    fig = px.bar(
+        df_activities,
+        x="facilitator_role",
+        y="total_hours",
+        title=_("Activity minutes by facilitator role"),
+        labels={
+            "facilitator_role": _("Facilitator role"),
+            "total_hours": _("Duration in hours"),
+        },
+    )
+
+    fig.update_layout(
+        title={
+            "y": 0.9,
+            "x": 0.5,
+            "xanchor": "center",
+            "yanchor": "top",
+        },
+        xaxis_title=_("Facilitator role"),
+        yaxis_title=_("Duration in hours"),
+        legend_title="",
+        template="plotly_dark",
+    )
+
+    return fig.to_html()
