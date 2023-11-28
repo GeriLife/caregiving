69974f67126389a968db4321f2dc33a5b822be25
Brylie Christopher Oxley <brylie@protonmail.com>
2023-11-28 05:21:49 +0200

    Refactor tests

--- homes/tests.py
+++ homes/tests.py
@@ -8,55 +8,37 @@ class HomeModelTests(TestCase):
     def setUp(self):
         # Create test data using factories
         self.home1 = HomeFactory(name="Home 1")
-        self.home2 = HomeFactory(name="Home 2")
 
-        self.resident1 = ResidentFactory(first_name="Alice")
-        self.resident2 = ResidentFactory(first_name="Bob")
-        self.resident3 = ResidentFactory(first_name="Charlie")
+        self.home_1_current_resident = ResidentFactory(first_name="Alice")
+        self.home_1_past_resident = ResidentFactory(first_name="Bob")
 
         ResidencyFactory(
-            resident=self.resident1,
+            resident=self.home_1_current_resident,
             home=self.home1,
             move_in="2020-01-01",
             move_out=None,
         )
         ResidencyFactory(
-            resident=self.resident2,
+            resident=self.home_1_past_resident,
             home=self.home1,
             move_in="2020-01-02",
             move_out="2020-02-01",
         )
-        ResidencyFactory(
-            resident=self.resident3,
-            home=self.home2,
-            move_in="2020-01-03",
-            move_out=None,
-        )
 
     def test_home_current_residencies(self):
-        # Testing Home 1
         current_residencies_home1 = self.home1.current_residencies
         self.assertEqual(current_residencies_home1.count(), 1)
         self.assertTrue(
-            current_residencies_home1.filter(resident=self.resident1).exists(),
-        )
-
-        # Testing Home 2
-        current_residencies_home2 = self.home2.current_residencies
-        self.assertEqual(current_residencies_home2.count(), 1)
-        self.assertTrue(
-            current_residencies_home2.filter(resident=self.resident3).exists(),
+            current_residencies_home1.filter(
+                resident=self.home_1_current_resident,
+            ).exists(),
         )
 
     def test_home_current_residencies_exclude_past_residencies(self):
         # Testing Home 1
         current_residencies_home1 = self.home1.current_residencies
         self.assertFalse(
-            current_residencies_home1.filter(resident=self.resident2).exists(),
-        )
-
-        # Testing Home 2
-        current_residencies_home2 = self.home2.current_residencies
-        self.assertFalse(
-            current_residencies_home2.filter(resident=self.resident1).exists(),
+            current_residencies_home1.filter(
+                resident=self.home_1_past_resident,
+            ).exists(),
         )
