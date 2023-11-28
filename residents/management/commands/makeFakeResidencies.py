69a03c0942fcda8d0c37fc2d26897c7e6336df4c
TBennur <tanay.bennur@gmail.com>
2023-11-28 14:52:27 -0500

    Added Residency Command Line Argument

---
+++ residents/management/commands/makeFakeResidencies.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Creates fake residencies. Requires residents and homes."
+
+    def add_arguments(self, parser):
+        parser.add_argument("num", type=int, help="Number of residencies to create")
+
+    def additional_logic(self):
+        return True
+
+    def handle(self, *args, **options):
+        # Parse Arguments
+        n = options["num"]
+        # Resident Number Verification
+        if n < 0 and self.additional_logic():
+            self.stdout.write("Invalid n. Please try again.")
+            return
+
+        self.stdout.write(f"Created {n} fake residencies.")
