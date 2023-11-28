8cdab7d7f9addf78b3e1390aa4f27c9e5743b2ae
TBennur <tanay.bennur@gmail.com>
2023-11-28 15:12:37 -0500

    Added Home Command Line Argument

--- homes/management/commands/makeFakeHomes.py
+++ homes/management/commands/makeFakeHomes.py
@@ -2,21 +2,18 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = "Creates fake residencies. Requires residents and homes."
+    help = "Creates fake homes."
 
     def add_arguments(self, parser):
-        parser.add_argument("num", type=int, help="Number of residencies to create")
-
-    def additional_logic(self):
-        return True
+        parser.add_argument("num", type=int, help="Number of homes to create")
 
     def handle(self, *args, **options):
         # Parse Arguments
         n = options["num"]
 
         # Home Number Verification
-        if n < 0 and self.additional_logic():
+        if n < 0:
             self.stdout.write("Invalid n. Please try again.")
             return
 
-        self.stdout.write(f"Created {n} fake residencies.")
+        self.stdout.write(f"Created {n} fake homes.")
