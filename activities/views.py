9118cc5fd3454d56bcd6123a7e4369db8e435804
Adrienne Li <adrienneli100@gmail.com>
2023-11-29 23:23:54 -0500

    Add resident activity after creating activity

--- activities/views.py
+++ activities/views.py
@@ -11,11 +11,38 @@ class ActivityFormView(FormView):
     # get the url to the activity list view
     success_url = reverse_lazy("activity-list-view")
 
+    def add_resident_activity(self, data):
+        for r in data["residents"]:
+            print("Placeholder")
+            # resident = r
+            # activity =
+            # try:
+            #     residency = Residency.objects.get(resident=r, move_out__isnull=True)
+            # except Residency.DoesNotExist:
+            #     print("Residency doesn't exist for this resident")
+            #     return
+            # home = residency.home
+            # activity_type = data[activity_type]
+            # activity_minutes = data[duration_minutes]
+            # caregiver_role = data[caregiver_role]
+
+            # resident_activity = ResidentActivity.objects.create(
+            #     resident=resident,
+            #     activity=activity,
+            #     residency=residency,
+            #     home=home,
+            #     activity_type=activity_type,
+            #     activity_minutes=activity_minutes,
+            #     caregiver_role=caregiver_role,
+            # )
+            # resident_activity.save()
+
     def form_valid(self, form):
         # save the form before redirecting to success URL
         # Note: this may be unnecessary,
         # but the form wasn't saving previously
         form.save()
+        self.add_resident_activity(form.cleaned_data)
 
         return super().form_valid(form)
 
--- metrics/models.py
+++ metrics/models.py
@@ -4,8 +4,6 @@ from homes.models import Home
 from residents.models import Resident
 from activities.models import Activity
 from residents.models import Residency
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class ResidentActivity(models.Model):
@@ -47,29 +45,29 @@ class ResidentActivity(models.Model):
         verbose_name = _("resident_activity")
         verbose_name_plural = _("resident_activities")
 
-    @receiver(post_save, sender=Activity)
-    def add_resident_activity(sender, instance, **kwargs):
-        residents = instance.residents.all().values_list("Resident", flat=True)
-        for r in residents:
-            resident = r
-            activity = instance.id
-            cur_residency = Residency.objects.filter(
-                resident=r,
-                move_out__isnull=True,
-            )
-            residency = cur_residency.id
-            home = cur_residency.home.id
-            activity_type = instance.activity_type
-            activity_minutes = instance.duration_minutes
-            caregiver_role = instance.caregiver_role
+    # @receiver(post_save, sender=Activity)
+    # def add_resident_activity(sender, instance, **kwargs):
+    #     residents = instance.residents.all().values_list("Resident", flat=True)
+    #     for r in residents:
+    #         resident = r
+    #         activity = instance.id
+    #         cur_residency = Residency.objects.filter(
+    #             resident=r,
+    #             move_out__isnull=True,
+    #         )
+    #         residency = cur_residency.id
+    #         home = cur_residency.home.id
+    #         activity_type = instance.activity_type
+    #         activity_minutes = instance.duration_minutes
+    #         caregiver_role = instance.caregiver_role
 
-            resident_activity = ResidentActivity.objects.create(
-                resident=resident,
-                activity=activity,
-                residency=residency,
-                home=home,
-                activity_type=activity_type,
-                activity_minutes=activity_minutes,
-                caregiver_role=caregiver_role,
-            )
-            resident_activity.save()
+    #         resident_activity = ResidentActivity.objects.create(
+    #             resident=resident,
+    #             activity=activity,
+    #             residency=residency,
+    #             home=home,
+    #             activity_type=activity_type,
+    #             activity_minutes=activity_minutes,
+    #             caregiver_role=caregiver_role,
+    #         )
+    #         resident_activity.save()
