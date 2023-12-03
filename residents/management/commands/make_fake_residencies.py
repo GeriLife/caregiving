da1495d5fef8279c53a13d4fedaab79ff68a0565
Adrienne Li <adrienneli100@gmail.com>
2023-12-03 17:48:12 -0500

    Added residency tests

--- residents/management/commands/make_fake_residencies.py
+++ residents/management/commands/make_fake_residencies.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from residents.factories import ResidentFactory
 
 
 class Command(BaseCommand):
@@ -7,16 +8,26 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("num", type=int, help="Number of residencies to create")
 
-    def additional_logic(self):
-        return True
-
     def handle(self, *args, **options):
-        # Parse Arguments
-        n = options["num"]
-
-        # Home Number Verification
-        if n < 0 and self.additional_logic():
-            self.stdout.write("Invalid n. Please try again.")
+        # Resident Number Verification
+        if options["num"] >= 0:
+            for _ in range(options["num"]):
+                ResidentFactory.create()
+            self.stdout.write(f"Created {options['num']} fake residencies.")
             return
 
-        self.stdout.write(f"Created {n} fake residencies.")
+        self.stdout.write("Invalid n. Please try again.")
+
+    # def additional_logic(self):
+    #     return True
+
+    # def handle(self, *args, **options):
+    #     # Parse Arguments
+    #     n = options["num"]
+
+    #     # Home Number Verification
+    #     if n < 0 and self.additional_logic():
+    #         self.stdout.write("Invalid n. Please try again.")
+    #         return
+
+    #     self.stdout.write(f"Created {n} fake residencies.")
