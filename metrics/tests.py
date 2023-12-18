12f1d7f5ff794d6a8bdd6164687a5980b60036dc
Brylie Christopher Oxley <brylie@protonmail.com>
2023-12-18 21:38:45 +0200

    Fix test_add_activity_adds_resident_activity

--- metrics/tests.py
+++ metrics/tests.py
@@ -1,8 +1,11 @@
+from http import HTTPStatus
 from django.test import TestCase
+
+from activities.models import Activity
 from .models import ResidentActivity
 from homes.factories import HomeFactory
 from residents.factories import ResidentFactory, ResidencyFactory
-from datetime import datetime
+from datetime import date
 from django.urls import reverse
 
 
@@ -32,16 +35,44 @@ class ResidentActivityTestCase(TestCase):
 
     def test_add_activity_adds_resident_activity(self):
         """When activity is added, resident activity is added."""
+        # activity count pre
+        activity_count_pre = Activity.objects.all().count()
+        # pre count
+        resident_activity_count_pre = ResidentActivity.objects.all().count()
+
         self.data = {
-            "residents": self.resident1,
-            "activity_type": "outdoor",
-            "date": datetime.now(),
+            "residents": [self.resident1.id],
+            "activity_type": Activity.ActivityTypeChoices.OUTDOOR,
+            # get the current date
+            "date": date.today(),
             "duration_minutes": 30,
-            "caregiver_role": "staff",
+            "caregiver_role": Activity.CaregiverRoleChoices.NURSE,
         }
         response = self.client.post(
             reverse("activity-form-view"),
             self.data,
-            content_type="application/x-www-form-urlencoded",
         )
-        print(response)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+
+        # activity count post
+        activity_count_post = Activity.objects.all().count()
+
+        expected_activity_count_post = 1
+
+        self.assertEqual(activity_count_post, expected_activity_count_post)
+
+        # ResidentActivity post count
+        resident_activity_count_post = ResidentActivity.objects.all().count()
+
+        expected_resident_activity_count_post = 1
+
+        self.assertEqual(
+            resident_activity_count_post,
+            expected_resident_activity_count_post,
+        )
+
+        # Activity pre count should be less than post count
+        self.assertLess(activity_count_pre, activity_count_post)
+
+        # ResidentActivity pre count should be less than post count
+        self.assertLess(resident_activity_count_pre, resident_activity_count_post)
