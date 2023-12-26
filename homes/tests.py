04fd3c10e7fa672481850a86f0c04db49dfacaf3
Brylie Christopher Oxley <brylie@protonmail.com>
2023-12-26 12:52:23 +0200

    Add test coverage for Home model methods

--- homes/tests.py
+++ homes/tests.py
@@ -2,8 +2,11 @@ from io import StringIO
 from django.core.management import call_command
 from django.test import TestCase
 from django.core.management.base import CommandError
+from django.utils import timezone
 
-from activities.factories import ActivityFactory
+from metrics.factories import ResidentActivityFactory
+from metrics.models import ResidentActivity
+from residents.models import Residency, Resident
 
 from .factories import HomeFactory
 from .models import Home
@@ -26,7 +29,7 @@ class HomeModelTests(TestCase):
             move_in="2020-01-01",
             move_out=None,
         )
-        ResidencyFactory(
+        self.home_1_current_resident_active_residency = ResidencyFactory(
             resident=self.home_1_current_resident_active,
             home=self.home1,
             move_in="2020-01-01",
@@ -39,8 +42,31 @@ class HomeModelTests(TestCase):
             move_out="2020-02-01",
         )
 
-        self.home_1_current_resident_active_activity = ActivityFactory(
-            residents=[self.home_1_current_resident_active],
+        today = timezone.now()
+
+        self.home_1_current_resident_active_resident_activity = ResidentActivityFactory(
+            resident=self.home_1_current_resident_active,
+            activity_date=today,
+            home=self.home1,
+            residency=self.home_1_current_resident_active_residency,
+        )
+
+    def test_mock_data(self):
+        self.assertEqual(Home.objects.count(), 1)
+        self.assertEqual(Residency.objects.count(), 3)
+        self.assertEqual(Resident.objects.count(), 3)
+        self.assertEqual(ResidentActivity.objects.count(), 1)
+
+        # assert home_1_current_resident_active in self.home_1_current_resident_active_activity.residents
+        self.assertEqual(
+            self.home_1_current_resident_active,
+            self.home_1_current_resident_active_resident_activity.resident,
+        )
+        # assert self.home_1_current_resident_active.recent_activity_count == 1
+        expected_recent_activity_count = 1
+        self.assertEqual(
+            self.home_1_current_resident_active.get_recent_activity_count(),
+            expected_recent_activity_count,
         )
 
     def test_home_current_residents(self):
@@ -62,6 +88,88 @@ class HomeModelTests(TestCase):
         self.assertNotIn(
             self.home_1_past_resident,
             current_residents_home1,
+        )
+
+    def test_residents_with_recent_activity_counts(self):
+        residents_with_recent_activity_counts = (
+            self.home1.residents_with_recent_activity_counts.all()
+        )
+
+        expected_residents_with_recent_activity_counts = [
+            {
+                "resident": self.home_1_current_resident_inactive,
+                "recent_activity_count": 0,
+            },
+            {
+                "resident": self.home_1_current_resident_active,
+                "recent_activity_count": 1,
+            },
+        ]
+
+        # get the index and item for each expected_residents_with_recent_activity_counts
+        for index, actual_resident in enumerate(residents_with_recent_activity_counts):
+            expected_resident = expected_residents_with_recent_activity_counts[index]
+            self.assertEqual(
+                actual_resident,
+                expected_resident["resident"],
+            )
+
+            self.assertEqual(
+                actual_resident.get_recent_activity_count(),
+                expected_resident["recent_activity_count"],
+            )
+
+    def test_home_resident_counts_by_activity_level(self):
+        home1_resident_counts_by_activity_level = (
+            self.home1.resident_counts_by_activity_level
+        )
+
+        expected_resident_counts_by_activity_level = {
+            "total_count": 2,
+            "inactive_count": 1,
+            "low_active_count": 1,
+            "good_active_count": 0,
+            "high_active_count": 0,
+            "inactive_percent": 50.0,
+            "low_active_percent": 50.0,
+            "good_active_percent": 0,
+            "high_active_percent": 0,
+        }
+        self.assertEqual(
+            home1_resident_counts_by_activity_level,
+            expected_resident_counts_by_activity_level,
+        )
+
+    def test_home_resident_counts_by_activity_level_chart_data(self):
+        home1_resident_counts_by_activity_level_chart_data = (
+            self.home1.resident_counts_by_activity_level_chart_data
+        )
+
+        expected_resident_counts_by_activity_level_chart_data = [
+            {
+                "activity_level_label": "Inactive",
+                "activity_level_class": "danger",
+                "value": 50.0,
+            },
+            {
+                "activity_level_label": "Low",
+                "activity_level_class": "warning",
+                "value": 50.0,
+            },
+            {
+                "activity_level_label": "Moderate",
+                "activity_level_class": "success",
+                "value": 0.0,
+            },
+            {
+                "activity_level_label": "High",
+                "activity_level_class": "warning",
+                "value": 0.0,
+            },
+        ]
+        self.assertEqual(
+            home1_resident_counts_by_activity_level_chart_data,
+            expected_resident_counts_by_activity_level_chart_data,
         )
 
 
