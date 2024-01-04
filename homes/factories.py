bfbf0fffe682c941e2957da722dd687174b460af
Brylie Christopher Oxley <brylie@protonmail.com>
2024-01-04 23:45:01 +0200

    Add HomeGroupFactory

--- homes/factories.py
+++ homes/factories.py
@@ -1,10 +1,29 @@
 import factory
 from factory import Sequence
 
+from .models import Home, HomeGroup, HomeUserRelation
+
 
 class HomeFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = "homes.Home"
+        model = Home
         django_get_or_create = ("name",)
 
     name: str = Sequence(lambda n: f"Home {n}")
+
+
+class HomeGroupFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = HomeGroup
+        django_get_or_create = ("name",)
+
+    name: str = Sequence(lambda n: f"Home Group {n}")
+
+
+class HomeUserRelationFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = HomeUserRelation
+        django_get_or_create = ("home", "user")
+
+    home: Home = factory.SubFactory(HomeFactory)
+    user: Home = factory.SubFactory(HomeFactory)
