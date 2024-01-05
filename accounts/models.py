049c1211d65ac4e742b955319c2d63d92954e8a7
Brylie Christopher Oxley <brylie@protonmail.com>
2024-01-05 23:34:53 +0200

    Add User.can_manage_residents with test

--- accounts/models.py
+++ accounts/models.py
@@ -29,3 +29,27 @@ class User(AbstractUser):
         are associated with at least one home.
         """
         return self.is_superuser or self.homes.exists()
+
+    def can_manage_residents(self, resident_ids: list[int]) -> bool:
+        """Return True if the user can manage the residents.
+
+        A user can manage the residents if they are a superuser or if
+        they are associated with all of the residents' homes.
+        """
+        from residents.models import Resident
+        from homes.models import HomeUserRelation
+
+        if self.is_superuser:
+            return True
+
+        residents = Resident.objects.filter(id__in=resident_ids)
+
+        for resident in residents:
+            # Check if the user is associated with the resident's home
+            if HomeUserRelation.objects.filter(
+                home=resident.current_home,
+                user=self,
+            ).exists():
+                return True
+
+        return False
